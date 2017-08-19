74a3007ee61dd3a5afcf919de90838ddcc58edf9
willdavis <willdavis84@gmail.com>
2017-08-18 18:13:23 -0700

    bugfix in juggle pattern

--- src/patterns/juggle.h
+++ src/patterns/juggle.h
@@ -7,9 +7,9 @@ void juggle() {
 
   byte dothue = 0;
   for(int i = 0; i < 8; i++) {
-    tail_leds[beatsin16(i+7,0,SHORT_LENGTH_LEDS)] |= CHSV(dothue, 200, 255);
-    body_leds[beatsin16(i+7,0,LONG_LENGTH_LEDS)] |= CHSV(dothue, 200, 255);
-    lips_leds[beatsin16(i+7,0,MEDIUM_LENGTH_LEDS)] |= CHSV(dothue, 200, 255);
+    tail_leds[beatsin16(i+7,0,SHORT_LENGTH_LEDS-1)] |= CHSV(dothue, 200, 255);
+    body_leds[beatsin16(i+7,0,LONG_LENGTH_LEDS-1)] |= CHSV(dothue, 200, 255);
+    lips_leds[beatsin16(i+7,0,MEDIUM_LENGTH_LEDS-1)] |= CHSV(dothue, 200, 255);
     dothue += 32;
   }
 
